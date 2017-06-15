d42254c00d62652c778a251e0f1b6139c822e862
Daniel <eggpie@sjtu.edu.cn>
2017-06-14 23:41:29 -0700

    change parameters

--- tcpclient.c
+++ tcpclient.c
@@ -28,7 +28,7 @@ void error(char *msg) {
 
 void estimiating(double *y_s, double *y_var, double *y_i, double *y_up){
 	double alpha = 0.1;
-	double beta = 0.2；
+    double beta = 0.2;
 	double kappa = 0.5;
 	*y_var = (1 - beta) * (*y_var) + beta * abs(*y_s - *y_i);
 	*y_s = (1 - alpha) * (*y_s) + alpha * (*y_i);
